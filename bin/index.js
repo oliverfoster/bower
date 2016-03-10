3e78ab2b6d5c3352268bf944fbb6c3c0923842be
Oliver Foster <oliver.foster@kineo.com>
2016-03-10 17:08:35 +0000

    Updates

--- bin/index.js
+++ bin/index.js
@@ -1,36 +1,27 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
-var app = express();
+var path = require("path");
+global.app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static( path.join(__dirname, '../public')));
 
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname ,'../views'));
 
-app.engine('handlebars', exphbs({
+app.engine('.hbs', exphbs({
 	extname: ".hbs"
 }));
-app.set('view engine', 'handlebars');
-
+app.set('view engine', '.hbs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
 
-app.get('/packages', function(request, response) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM test_table', function(err, result) {
-			done();
-			if (err) { 
-				console.error(err); response.send("Error " + err); 
-			} else { 
-				response.render('pages/db', {results: result.rows} ); 
-			}
-		});
-	});
-})
+var packages = require('../lib/routes/packages');
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
